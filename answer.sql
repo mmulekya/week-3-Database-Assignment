7bec39eb9771f423580b0e579a3ea8a308449a33
mmulekya <kizitohamisi61@gmail.com>
2026-09-19 16:53:52 +0300

    Create school database and student table with data
    Created a database and student table, inserted records, and updated a student's age.

--- answer.sql
+++ answer.sql
@@ -1,1 +1,28 @@
+-- week 3 Database Assignment
+-- Student Table
 
+--Step One 
+ CREATE DATABASE school;
+USE school;
+
+-- Question 1
+CREATE TABLE student (
+    id INT PRIMARY KEY,
+    fullName VARCHAR(100),
+    age INT
+);
+
+-- Question 2
+INSERT INTO student (id, fullName, age)
+VALUES
+(1, 'John Kamau', 19),
+(2, 'Mary Wanjiku', 18),
+(3, 'David Otieno', 21);
+
+-- Question 3
+UPDATE student
+SET age = 20
+WHERE id = 2;
+
+-- Finally is to check the results
+SELECT * FROM student;
